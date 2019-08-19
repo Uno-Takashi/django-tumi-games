017dc52cb43334eee27ca92e7a6edecefb09feaa
euno <eunoe.eng@gmail.com>
2019-08-19 14:06:46 +0900

    firlst

--- steam_tumi_games/app/views.py
+++ steam_tumi_games/app/views.py
@@ -9,20 +9,26 @@ from allauth.socialaccount.models import SocialAccount
 from django.views.generic import *
 from django.contrib.auth.models import User
 
+common_datas={"site_name":"積みゲーで遊ぼう"}
 
 class HomeView(TemplateView):
-    template_name = "app/index.html"
+    template_name = "app/index_logout.html"
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        context["title"] = "Home Page"
-        context["year"]=datetime.now().year
+
         if self.request.user.is_authenticated:
+            self.template_name = "app/index_login.html"
+
             for account in SocialAccount.objects.filter(user=self.request.user):
                 provider_account = account.get_provider_account()
+            
             print(provider_account)
 
+        context["title"] = "Home"
+        context["year"]=datetime.now().year
+        context["common_datas"]=common_datas
         return context
 
 
